8e26a8e7f1d11aa137ab1e3e6b8ddeceafe20344
baizon <umstaedter@gmail.com>
2016-01-31 10:19:54 +0100

    Added own custom shortcuts and added download to UMS.

--- upgrade-ums.sh
+++ upgrade-ums.sh
@@ -3,17 +3,20 @@
 LOCATION="$HOME/Downloads"
 
 read -p "Enter UMS version: " VERSION
-echo ""
 
 UMSFILE="$LOCATION/UMS-$VERSION-Java8.tgz" 
 
+echo "Downloading UMS:"
+wget http://sourceforge.net/projects/unimediaserver/files/Official%20Releases/Linux/UMS-$VERSION-Java8.tgz -P $LOCATION
 if [ -f $UMSFILE ]; then
  echo "Extracting files:"
- tar zxvf $UMSFILE -C $LOCATION/
+ tar zxvf $UMSFILE -C $LOCATION
  echo "Removing /opt/ums/:"
  sudo rm -rvf /opt/ums/
  echo "Moving files from $HOME/Downloads/ums-$VERSION/ to /opt/ums/:"
- sudo mv $LOCATION/ums-$VERSION/ /opt/ums/
+ sudo mv -v $LOCATION/ums-$VERSION/ /opt/ums/
+ echo "Removing temporary files:"
+ rm -rvf $UMSFILE
  echo "All done. Exiting."
 else
  echo "File $UMSFILE not found. Aborting."
