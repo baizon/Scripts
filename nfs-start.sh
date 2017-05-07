30da33cff9be12fad9ef5da50a09e130c12af560
baizon <umstaedter@gmail.com>
2017-05-07 08:33:36 +0200

    Added checks, refactoring

--- nfs-start.sh
+++ nfs-start.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
 MOUNT_OPTIONS="192.168.0.1/24(ro,no_subtree_check,insecure,sync)"
-MOVIES_STRING="/srv/nfs/Movies"
-TVSHOWS_STRING="/srv/nfs/TVShows"
+MOVIES_DISK_PATH="/run/media/baizon/Data/Movies"
+TVSHOWS_DISK_PATH="/run/media/baizon/Data/TVShows"
+MOVIES_NFS_PATH="/srv/nfs/Movies"
+TVSHOWS_NFS_PATH="/srv/nfs/TVShows"
 EXPORTS_FILE_PATH="/etc/exports"
+
+addNfsDir() {
+  if [ ! -d "$1" ]; then
+    echo "Creating directory $1"
+    sudo mkdir -p $1
+  fi
+}
+
+mountNfsDir() {
+echo "Mounting $2 directory..."
+sudo mount --bind $1 $2
+}
 
 addExportsEntry() {
   echo "Checking for entry $1 within /etc/exports"
@@ -17,17 +31,15 @@ addExportsEntry() {
   fi
 }
 
-echo "Creating directory $MOVIES_STRING"
-sudo mkdir -p $MOVIES_STRING
-echo "Mounting Movies directory..."
-sudo mount --bind /run/media/baizon/Data/Movies $MOVIES_STRING
-addExportsEntry $MOVIES_STRING
+# Adding Movies NFS
+addNfsDir $MOVIES_NFS_PATH
+mountNfsDir $MOVIES_DISK_PATH $MOVIES_NFS_PATH
+addExportsEntry $MOVIES_NFS_PATH
 
-echo "Creating directory $TVSHOWS_STRING"
-sudo mkdir -p $TVSHOWS_STRING
-echo "Mounting TV Shows directory..."
-sudo mount --bind /run/media/baizon/Data/TVShows $TVSHOWS_STRING
-addExportsEntry $TVSHOWS_STRING
+# Adding TV Shows NFS
+addNfsDir $TVSHOWS_NFS_PATH
+mountNfsDir $TVSHOWS_DISK_PATH $TVSHOWS_NFS_PATH
+addExportsEntry $TVSHOWS_NFS_PATH
 
 echo "Starting NFS service..."
 sudo systemctl start nfs-server
