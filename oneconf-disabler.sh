066f807ff721acbe311fdc2943bcf93116f46d86
baizon <umstaedter@gmail.com>
2018-07-20 22:01:17 +0200

    Removed unused scripts

--- oneconf-disabler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo sh -c "chmod a-x /usr/share/oneconf/oneconf-service"
-sudo sh -c "chmod a-x /usr/share/oneconf/oneconf-query"
-sudo sh -c "chmod a-x /usr/share/oneconf/oneconf-update"
