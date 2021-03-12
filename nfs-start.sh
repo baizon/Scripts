95fe862719ab8c944aa3d5d6dcf8295d255de2ea
baizon <umstaedter@gmail.com>
2021-03-12 10:53:32 +0100

    Changed nfs start and stop logic

--- nfs-start.sh
+++ nfs-start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # --- CONFIG ---------------------------------------------------
-MOUNT_OPTIONS="192.168.0.1/24(ro,no_subtree_check,insecure,sync)"
-MOVIES_DISK_PATH="/run/media/baizon/Data/Movies"
-TVSHOWS_DISK_PATH="/run/media/baizon/Data/TVShows"
-MOVIES_NFS_PATH="/srv/nfs/Movies"
-TVSHOWS_NFS_PATH="/srv/nfs/TVShows"
+READ_MOUNT_OPTIONS="192.168.0.221/24(ro,no_subtree_check,sync)"
+WRITE_MOUNT_OPTIONS="192.168.0.221/24(rw,no_subtree_check,sync)"
+MOVIES_PATH="/mnt/data/Movies"
+PICTURES_PATH="/mnt/data/Pictures"
+TVSHOWS_PATH="/mnt/data/TVShows"
+UPLOAD_PATH="/mnt/data/Upload"
 EXPORTS_FILE_PATH="/etc/exports"
 # --------------------------------------------------------------
 
@@ -16,33 +17,33 @@ addNfsDir() {
   fi
 }
 
-mountNfsDir() {
-echo "Mounting $2 directory..."
-sudo mount --bind $1 $2
-}
-
 addExportsEntry() {
   echo "Checking for entry $1 within /etc/exports"
   if $(grep -q "$1" "$EXPORTS_FILE_PATH") ; then
     echo "Entry found, nothing to do."
   else
     echo "Adding entry to /etc/exports"
-    sudo sh -c "echo $1 '$MOUNT_OPTIONS' >> $EXPORTS_FILE_PATH"
+    sudo sh -c "echo $1 '$2' >> $EXPORTS_FILE_PATH"
     echo "Running exportfs to update..."
     sudo exportfs -arv
   fi
 }
 
-# Adding Movies NFS
-addNfsDir $MOVIES_NFS_PATH
-mountNfsDir $MOVIES_DISK_PATH $MOVIES_NFS_PATH
-addExportsEntry $MOVIES_NFS_PATH
+echo "Adding Movies NFS (read)"
+addNfsDir $MOVIES_PATH
+addExportsEntry $MOVIES_PATH $READ_MOUNT_OPTIONS
 
-# Adding TV Shows NFS
-addNfsDir $TVSHOWS_NFS_PATH
-mountNfsDir $TVSHOWS_DISK_PATH $TVSHOWS_NFS_PATH
-addExportsEntry $TVSHOWS_NFS_PATH
+echo "Adding Pictures NFS (read)"
+addNfsDir $PICTURES_PATH
+addExportsEntry $PICTURES_PATH $READ_MOUNT_OPTIONS
+
+echo "Adding TV Shows NFS (read)"
+addNfsDir $TVSHOWS_PATH
+addExportsEntry $TVSHOWS_PATH $READ_MOUNT_OPTIONS
+
+echo "Adding Upload NFS (write)"
+addNfsDir $UPLOAD_PATH
+addExportsEntry $UPLOAD_PATH $WRITE_MOUNT_OPTIONS
 
 echo "Starting NFS service..."
-sudo systemctl start nfs-server
-read -p "All done. Press any key to continue..." input
+sudo service nfs-kernel-server start
--- nfs-stop.sh
+++ nfs-stop.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-MOVIES_STRING="/srv/nfs/Movies"
-TVSHOWS_STRING="/srv/nfs/TVShows"
+# --- CONFIG ----------------------
+MOVIES_STRING="/mnt/data/Movies"
+PICTURES_STRING="/mnt/data/Pictures"
+TVSHOWS_STRING="/mnt/data/TVShows"
+UPLOAD_STRING="/mnt/data/Upload"
+# ---------------------------------
 
 echo "Stopping NFS service..."
-sudo systemctl stop nfs-server
+sudo service nfs-kernel-server stop
 
 echo "Unmounting Movies directory..."
 sudo umount $MOVIES_STRING
-echo "Removing $MOVIES_STRING"
-sudo rm -rf $MOVIES_STRING
+
+echo "Unmounting Pictures directory..."
+sudo umount $PICTURES_STRING
 
 echo "Unmounting TV Shows directory..."
 sudo umount $TVSHOWS_STRING
-echo "Removing $TVSHOWS_STRING"
-sudo rm -rf $TVSHOWS_STRING
+
+echo "Unmounting Upload directory..."
+sudo umount $UPLOAD_STRING
+
