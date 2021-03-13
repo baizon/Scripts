7c106981f42f4edd6f44710b85f6d00e9641bde9
baizon <umstaedter@gmail.com>
2021-03-13 16:12:09 +0100

    Fixed naming

--- nfs-umount.sh
+++ nfs-umount.sh
@@ -7,14 +7,14 @@ TVSHOWS_LOCAL_PATH="/home/baizon/NFS/TVShows"
 UPLOAD_LOCAL_PATH="/home/baizon/NFS/Upload"
 # --------------------------------------------------------------
 
-mountNfsDir() {
-  echo "Mounting $2 from IP $1 to $3 directory..."
+umountNfsDir() {
+  echo "Unmounting $1 directory..."
   sudo umount $1
 }
 
-mountNfsDir $MOVIES_LOCAL_PATH
-mountNfsDir $PICTURES_LOCAL_PATH
-mountNfsDir $TVSHOWS_LOCAL_PATH
-mountNfsDir $UPLOAD_LOCAL_PATH
+umountNfsDir $MOVIES_LOCAL_PATH
+umountNfsDir $PICTURES_LOCAL_PATH
+umountNfsDir $TVSHOWS_LOCAL_PATH
+umountNfsDir $UPLOAD_LOCAL_PATH
 
 read -p "All done. Press any key to continue..." input
